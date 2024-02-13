b34c9c54655122e22d4e8aa120ed520620d4dcd6
Rayara <ravtra@hotmail.com>
2024-02-13 16:39:13 -0300

    alteracoes no testemunho

--- src/Componentes/Principal5/Principal5.js
+++ src/Componentes/Principal5/Principal5.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Principal5 = () => {
+function Principal5 (){
 
   return (
     <div className='prin-5'>
@@ -8,10 +8,17 @@ const Principal5 = () => {
             <p>TESTEMONIALS</p>
             <h1>WHAT THEY SAY</h1>
         </div>
-        <div className='slider'>
-            <div className='container'></div>
-            <h1 className='title'> Testemonials</h1>
-        </div>
+		<div className='conteiner'>
+			<div className='texto-testemunho'>
+				<img src='./imagens/aspas.png'></img>
+				<h1>“Donec et nunc massa. Nullam non felis dignissim, dapibus turpis semper, vulputate lorem. Nam volutpat posuere tellus, in porttitor justo interdum nec. Aenean in dapibus risus, in euismod ligula. Aliquam vel scelerisque elit.”</h1>
+				<h2>David Eigenberg</h2>
+				<p>Crypto Manager</p>
+			</div>
+			<div className='imagem-p5'>
+				<img className='pessoa-dep' src='./imagens/testimonials-01.jpg'/>
+			</div>
+		</div>
     </div>
   );
 };
